727968ce42741d0fe514a8e67ade8b746153249c
Jessica Alejandra Sánchez <42454185+alejajessi@users.noreply.github.com>
2020-03-05 15:09:13 -0500

    add linter fix (#7)

--- test/GithubApi.Authentication.test.js
+++ test/GithubApi.Authentication.test.js
@@ -4,27 +4,26 @@ const { expect } = require('chai');
 
 const urlBase = 'https://api.github.com';
 const githubUserName = 'alejajessi';
-const textDescription="This is a Workshop about Api Testing in JavaScript";
+const textDescription = 'This is a Workshop about Api Testing in JavaScript';
 const repository = 'workshop-api-testing-js';
 
 describe('Github Api Test', () => {
   describe('Authentication', () => {
     it('Via OAuth2 Tokens by Header', async () => {
-        const response = await agent.get(`${urlBase}/repos/${githubUserName}/${repository}`)
+      const response = await agent.get(`${urlBase}/repos/${githubUserName}/${repository}`)
         .auth('token', process.env.ACCESS_TOKEN)
         .set('User-Agent', 'agent');
-      
+
       expect(response.status).to.equal(statusCode.OK);
       expect(response.body.description).equal(textDescription);
     });
 
-    it('Via OAuth2 Tokens by parameter', () =>
-      agent.get(`${urlBase}/repos/${githubUserName}/${repository}`)
-        .query(`access_token=${process.env.ACCESS_TOKEN}`)
-        .set('User-Agent', 'agent')
-        .then((response) => {
-            expect(response.status).to.equal(statusCode.OK);
-            expect(response.body.description).equal(textDescription);
-        }));
+    it('Via OAuth2 Tokens by parameter', () => agent.get(`${urlBase}/repos/${githubUserName}/${repository}`)
+      .query(`access_token=${process.env.ACCESS_TOKEN}`)
+      .set('User-Agent', 'agent')
+      .then((response) => {
+        expect(response.status).to.equal(statusCode.OK);
+        expect(response.body.description).equal(textDescription);
+      }));
   });
 });
